3a5ea51b1484cc41469404308bc1879c3d922b93
katherinefont <77942207+katherinefont@users.noreply.github.com>
2022-02-08 14:52:03 -0400

    Subiendo proyecto

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.num1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnSuma.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSuma.Location = new System.Drawing.Point(62, 135);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(75, 43);
+            this.btnSuma.TabIndex = 0;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = false;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnResta.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResta.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnResta.Location = new System.Drawing.Point(143, 135);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(75, 43);
+            this.btnResta.TabIndex = 1;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = false;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnMultiplicacion.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicacion.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnMultiplicacion.Location = new System.Drawing.Point(224, 135);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(75, 43);
+            this.btnMultiplicacion.TabIndex = 1;
+            this.btnMultiplicacion.Text = "*";
+            this.btnMultiplicacion.UseVisualStyleBackColor = false;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnDivision.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivision.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDivision.Location = new System.Drawing.Point(305, 135);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(75, 43);
+            this.btnDivision.TabIndex = 1;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = false;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(66, 82);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(142, 20);
+            this.txtnum1.TabIndex = 2;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(234, 82);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(141, 20);
+            this.txtnum2.TabIndex = 2;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(63, 245);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(144, 20);
+            this.txtResultado.TabIndex = 2;
+            // 
+            // num1
+            // 
+            this.num1.AutoSize = true;
+            this.num1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(62, 31);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(146, 23);
+            this.num1.TabIndex = 3;
+            this.num1.Text = "Primer Número";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(230, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Segundo Número";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Resultado";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(266, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 51);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(446, 353);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiplicacion);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSuma);
+            this.Name = "Form1";
+            this.Text = "Calculadora Sencilla";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnMultiplicacion;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label num1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Calculadora/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtnum1.Text)
+                || string.IsNullOrEmpty(txtnum2.Text))
+            { 
+                MessageBox.Show("Complete los campos faltantes", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+
+            else
+            
+            { 
+               var num1 = int.Parse(txtnum1.Text);
+               var num2 = int.Parse(txtnum2.Text);
+               var Resultado = (num1 + num2);
+
+               txtResultado.Text = Convert.ToString(Resultado);
+            }
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtnum1.Text)
+               || string.IsNullOrEmpty(txtnum2.Text))
+            {
+                MessageBox.Show("Complete los campos faltantes", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            else
+            { 
+               var num1 = int.Parse(txtnum1.Text);
+               var num2 = int.Parse(txtnum2.Text);
+               var Resultado = (num1 - num2);
+
+               txtResultado.Text = Convert.ToString(Resultado);
+            }
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtnum1.Text)
+               || string.IsNullOrEmpty(txtnum2.Text))
+            {
+                MessageBox.Show("Complete los campos faltantes", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            else
+            { 
+            var num1 = int.Parse(txtnum1.Text);
+            var num2 = int.Parse(txtnum2.Text);
+            var Resultado = (num1 * num2);
+
+            txtResultado.Text = Convert.ToString(Resultado);
+            }
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtnum1.Text)
+               || string.IsNullOrEmpty(txtnum2.Text))
+            {
+                MessageBox.Show("Complete los campos faltantes", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            else
+            { 
+            var num1 = int.Parse(txtnum1.Text);
+            var num2 = int.Parse(txtnum2.Text);
+            var Resultado = (num1 / num2);
+
+            txtResultado.Text = Convert.ToString(Resultado);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtnum1.Clear();
+            txtnum2.Clear();
+            txtResultado.Clear();
+        }
+    }
+}
